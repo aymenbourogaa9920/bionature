fcc0ede43e97d618e7147bbd8fb58c40ad93a2fe
Aymen Bourogaa <aymenbourogaa5@gmail.com>
2025-01-16 14:48:43 +0100

    add prices

--- assets/js/translation.js
+++ assets/js/translation.js
@@ -15,9 +15,23 @@ const translations = {
     Avec des ingrédients soigneusement sélectionnés, nous vous garantissons efficacité, sécurité et sérénité."`,
     portfolio_title: "Portfolio",
     portfolio_paragraph: "Voici quelques photos de nos produits.",
+    portfolio_item_1: "Prix : 27 TND",
+    portfolio_item_2: "Prix : 27 TND",
+    portfolio_item_3: "Prix : 27 TND",
     contact_title: "Contact",
     contact_paragraph: "Pour commander, contactez-nous.",
-    footer_about: "© Bio Nature, Tous droits réservés."
+    contact_label_name: "Nom & Prénom",
+    contact_label_email: "Numéro de téléphone",
+    contact_label_subject: "Adresse",
+    contact_button: "Envoyer un message",
+    footer_about: "© Bio Nature, Tous droits réservés.",
+    loading_message: "Chargement",
+    error_message: "Une erreur est survenue. Veuillez réessayer.",
+    success_message: "Votre message est bien reçu!",
+    info_address_title: "Adresse",
+    info_address_detail: "Tunis, Tunisia",
+    info_phone_title: "Appelez-nous",
+    info_phone_detail: "+216 50 024 960"
   },
   tn: {
     nav_home: "الرئيسية",
@@ -34,9 +48,23 @@ const translations = {
                                      تقديم حل طبيعي 100٪، بدون مواد كيميائية، مناسب لجميع أفراد الأسرة مع مكونات مختارة بعناية، نضمن لكم الفعالية والأمان والراحة`,
     portfolio_title: "معرض الصور",
     portfolio_paragraph: "بعض الصور لمنتجاتنا.",
+    portfolio_item_1: "السعر: 27 دينار ",
+    portfolio_item_2: "السعر: 27 دينار ",
+    portfolio_item_3: "السعر: 27 دينار ",
     contact_title: "اتصل بنا",
     contact_paragraph: "للطلب، يرجى الاتصال بنا.",
-    footer_about: "© بيو ناتير، جميع الحقوق محفوظة."
+    contact_label_name: "الاسم و اللقب",
+    contact_label_email: "رقم الهاتف",
+    contact_label_subject: "العنوان",
+    contact_button: "إرسال الرسالة",
+    footer_about: "© بيو ناتير، جميع الحقوق محفوظة.",
+    loading_message: "جار التحميل",
+    error_message: "حدث خطأ. يرجى المحاولة مرة أخرى.",
+    success_message: "تم استلام رسالتك بنجاح!",
+    info_address_title: "العنوان",
+    info_address_detail: "تونس، تونس",
+    info_phone_title: "اتصل بنا",
+    info_phone_detail: "+216 50 024 960"
   }
 };
 
@@ -57,9 +85,26 @@ function translateContent(lang) {
 
   document.getElementById("portfolio-title").textContent = selectedLanguage.portfolio_title;
   document.getElementById("portfolio-paragraph").textContent = selectedLanguage.portfolio_paragraph;
+  document.querySelectorAll(".portfolio-info")[0].textContent = selectedLanguage.portfolio_item_1;
+  document.querySelectorAll(".portfolio-info")[1].textContent = selectedLanguage.portfolio_item_2;
+  document.querySelectorAll(".portfolio-info")[2].textContent = selectedLanguage.portfolio_item_3;
 
   document.getElementById("contact-title").textContent = selectedLanguage.contact_title;
   document.getElementById("contact-paragraph").textContent = selectedLanguage.contact_paragraph;
+  document.querySelector("label[for='name-field']").textContent = selectedLanguage.contact_label_name;
+  document.querySelector("label[for='email-field']").textContent = selectedLanguage.contact_label_email;
+  document.querySelector("label[for='subject-field']").textContent = selectedLanguage.contact_label_subject;
+  document.querySelector("button[type='submit']").textContent = selectedLanguage.contact_button;
+  // Contact Feedback Messages
+  document.querySelector(".loading").textContent = selectedLanguage.loading_message;
+  document.querySelector(".error-message").textContent = selectedLanguage.error_message;
+  document.querySelector(".sent-message").textContent = selectedLanguage.success_message;
+
+  // Info Section
+  document.querySelector(".info-item .bi-geo-alt + div h3").textContent = selectedLanguage.info_address_title;
+  document.querySelector(".info-item .bi-geo-alt + div p").textContent = selectedLanguage.info_address_detail;
+  document.querySelector(".info-item .bi-telephone + div h3").textContent = selectedLanguage.info_phone_title;
+  document.querySelector(".info-item .bi-telephone + div p").textContent = selectedLanguage.info_phone_detail;
 
   document.getElementById("footer-about").textContent = selectedLanguage.footer_about;
 }
